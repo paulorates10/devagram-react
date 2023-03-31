d96f47faee6c8f95d4e66a1ce03143e31e4f3a75
paulorates10 <126815846+paulorates10@users.noreply.github.com>
2023-03-31 18:59:57 -0300

    upload imagem

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,28 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Botao from '../componentes/botao'
+import { useRef, useState } from 'react'
 import Avatar from '../componentes/avatar'
+import Botao from '../componentes/botao'
+import {UploadImagem} from '../componentes/uploadImagem'
+
 
 export default function Home() {
+  const [imagem,setImagem]= useState(null);
+  const referenciaInput= useRef(null);
+
+  console.log(imagem);
+
   return (
       <>
         <h1>Ola mundo</h1>
+        <button onClick={()=>referenciaInput?.current.click()}>abrir seletor de arquivo</button>
+
+        <UploadImagem 
+          setImagem={setImagem} 
+          imagemPreview={imagem?.preview}
+          aoSetarAReferencia={(ref)=>referenciaInput.current=ref}
+        />
+
         <div style={{width:200}}>
           <Avatar/>
           <Botao texto={'Login'} cor='primario' manipularClique={()=>console.log('Botao clicado')}/>
