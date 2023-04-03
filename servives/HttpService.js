298f8f5b497fa31e71947375f7cd64e62c91e831
paulorates10 <126815846+paulorates10@users.noreply.github.com>
2023-04-03 12:56:16 -0300

    teste

--- servives/HttpService.js
+++ servives/HttpService.js
@@ -1,8 +1,8 @@
-import axios from "axios"
+import axios from "axios";
 
 
 export default class HttpService{
-    construtor(){
+    constructor(){
         this.axios=axios.create({
             baseURL: process.env.NEXT_PUBLIC_API_URL + '/api'
         });
